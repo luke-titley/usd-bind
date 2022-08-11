fe53a80269ed0326c2a045ba6856ccee6de6eebc
Luke Titley <from+badja@luketitley.com>
2022-08-11 21:46:36 +0200

    Make sure build file is working

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 export PATH=/root/.cargo/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/USD/lib:$LD_LIBRARY_PATH
-g++ --version
-#cd usd-rs && env CMAKE_PREFIX_PATH=/usr/local/USD cargo test
+#g++ --version
+cd usd-rs && env CMAKE_PREFIX_PATH=/usr/local/USD cargo test
